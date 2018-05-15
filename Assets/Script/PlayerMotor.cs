8c021cf824fd92ad162fcac621d7284806c5793b
Unknown <ugurtufekci.tobbetu@gmail.com>
2018-05-15 23:27:30 +0300

    Jumping and rotating animations

--- Assets/Script/PlayerMotor.cs
+++ Assets/Script/PlayerMotor.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     private const float LANE_DISTANCE = 3.0f;
+    private const float TURN_SPEED = 0.05f;
+
+    //Animation
+    private Animator anim;
+
     //Movement
     private CharacterController controller;
     private float jumpForce = 4.0f;
@@ -16,6 +21,7 @@ public class PlayerMotor : MonoBehaviour
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -43,17 +49,60 @@ public class PlayerMotor : MonoBehaviour
         //Calculate our move delta
         Vector3 moveVector = Vector3.zero;
         moveVector.x = (targetPosition - transform.position).normalized.x * speed;
-        moveVector.y = -0.1f;
+
+        bool isGrounded = IsGrounded();
+        anim.SetBool("Grounded",isGrounded);
+        //Calculate Y
+        if (isGrounded) // if grounded
+        {
+            verticalVelocity = -0.1f;
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Jump
+                anim.SetTrigger("Jump");
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= (gravity * Time.deltaTime);
+
+            //Fast Falling mechanic
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = -jumpForce;
+            }
+        }
+
+        moveVector.y = verticalVelocity;
         moveVector.z = speed;
 
         //Move the Player
         controller.Move(moveVector * Time.deltaTime);
 
+        //Rotate the player to where he is going
+        Vector3 dir = controller.velocity;
+        if(dir != Vector3.zero)
+        {
+            dir.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, dir, TURN_SPEED);
+        }
+        
     }
 
     private void MoveLane(bool goingRight)
     {
         desiredLane += (goingRight) ? 1 : -1;
         desiredLane = Mathf.Clamp(desiredLane, 0, 2); //edge controls
+    }
+
+    private bool IsGrounded()
+    {
+        Ray groundRay = new Ray(new Vector3(controller.bounds.center.x,(controller.bounds.center.y - controller.bounds.extents.y) +0.2f,controller.bounds.center.z),Vector3.down);
+        Debug.DrawRay(groundRay.origin, groundRay.direction, Color.cyan, 1.0f);
+
+        return Physics.Raycast(groundRay,0.2f+ 0.1f);
+       
     }
 }
