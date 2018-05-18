cf06166e5ab93ef6538084f7952336e5b2fa24b3
Unknown <ugurtufekci.tobbetu@gmail.com>
2018-05-19 00:35:54 +0300

    Beginning of Level Creation

---
+++ Assets/Script/PieceSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceSpawner : MonoBehaviour {
+
+    public PieceType type;
+    private Piece currentPiece;
+
+    public void Spawn()
+    {
+
+    }
+
+    public void DeSpawn()
+    {
+
+    }
+}
