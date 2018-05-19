845c5d4dd86a6ae8af891b5bcbef94e1b5cb5c24
Unknown <ugurtufekci.tobbetu@gmail.com>
2018-05-19 23:51:35 +0300

    Small fix
    Forget to uncomment a line.

--- Assets/Script/PieceSpawner.cs
+++ Assets/Script/PieceSpawner.cs
@@ -9,7 +9,7 @@ public class PieceSpawner : MonoBehaviour {
 
     public void Spawn()
     {
-        //  currentPiece = //get me a new piece from the pool
+        currentPiece = LevelManager.Instance.GetPiece(type, 0);
         currentPiece.gameObject.SetActive(true);
         currentPiece.transform.SetParent(transform, false);
     }
