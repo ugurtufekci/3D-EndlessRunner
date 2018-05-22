8c425e296955eb52491e4574046b6b3c6f55adf3
Unknown <ugurtufekci.tobbetu@gmail.com>
2018-05-22 10:39:58 +0300

    Level Manager ready!
    LevelManager can create levels with using segments.

--- Assets/Script/PlayerMotor.cs
+++ Assets/Script/PlayerMotor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    private const float LANE_DISTANCE = 3.0f;
+    private const float LANE_DISTANCE = 2.0f;
     private const float TURN_SPEED = 0.05f;
 
     //
